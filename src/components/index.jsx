65776b380b8eb6361a5d67816dd50ef16dea1691
fedevaz0412 <favazquez@correo.um.edu.uy>
2022-10-26 09:27:34 -0300

    Commit chat

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -5,3 +5,4 @@ export { Login } from "./login/login";
 export { Register } from "./login/register";
 export { InitialView } from "./init/initialView";
 export { ErrorMessage } from "./errorMessage";
+export { chat } from "./init/chats/chat";
